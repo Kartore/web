cdb538cae956d92a0ee171b37e701b7fc374ac87
NEKOYASAN <hello@nekoyasan.me>
2024-04-06 07:06:07 +0900

    fix: mode settings

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -12,41 +12,43 @@ const preview: Preview = {
     },
     viewport: {
       viewports: {
-        small: { name: "Small", styles: { width: "640px", height: "800px" } },
-        large: { name: "Large", styles: { width: "1024px", height: "1000px" } },
+        small: { name: 'Small', styles: { width: '640px', height: '800px' } },
+        large: { name: 'Large', styles: { width: '1024px', height: '1000px' } },
       },
     },
     backgrounds: {
       values: [
-        { name: "light", value: "#fff" },
-        { name: "dark", value: "#1E293B" },
+        { name: 'light', value: '#fff' },
+        { name: 'dark', value: '#1E293B' },
       ],
     },
     chromatic: {
-      mobile: {
-        viewport: "small",
+      modes: {
+        mobile: {
+          viewport: 'small',
+        },
+        desktop: {
+          viewport: 'large',
+        },
+        light: {
+          backgrounds: { value: '#fff' },
+          theme: 'light',
+        },
+        dark: {
+          backgrounds: { value: '#1E293B' },
+          theme: 'dark',
+        },
       },
-      desktop: {
-        viewport: "large",
-      },
-      light: {
-        backgrounds: { value: "#fff" },
-        theme: "light",
-      },
-      dark: {
-        backgrounds: { value: "#1E293B" },
-        theme: "dark",
-      },
-    }
+    },
   },
   decorators: [
     withThemeByClassName({
       themes: {
-        light: "light",
-        dark: "dark",
+        light: 'light',
+        dark: 'dark',
       },
-      defaultTheme: "dark",
-      parentSelector: "html",
+      defaultTheme: 'dark',
+      parentSelector: 'html',
     }),
   ],
 };
