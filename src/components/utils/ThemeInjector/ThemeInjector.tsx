0da1e1aaa95e41689045c607ffadfb425859d587
NEKOYASAN <hello@nekoyasan.me>
2024-04-07 04:40:18 +0900

    replace to themeInjector

---
+++ src/components/utils/ThemeInjector/ThemeInjector.tsx
@@ -0,0 +1,7 @@
+import { useThemeInjector } from '~/components/utils/ThemeInjector/ThemeInjector.hook.tsx';
+
+export const ThemeInjector = () => {
+  useThemeInjector();
+
+  return null;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { ThemeInjector } from '~/components/utils/ThemeInjector/ThemeInjector.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <ThemeInjector />
   </React.StrictMode>
 );
