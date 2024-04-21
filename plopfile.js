d84cb63ab7c4c96dd5be296fbf4f4be6b16a0855
NEKOYASAN <hello@nekoyasan.me>
2024-04-21 20:38:53 +0900

    add: component template

---
+++ plopfile.js
@@ -0,0 +1,39 @@
+export default function Plopfile(plop) {
+  plop.setGenerator('component', {
+    description: 'Create a new component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'parentPath',
+        message: 'src/components/{path please}',
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Component name',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/components/{{parentPath}}/{{name}}/{{name}}.tsx',
+        templateFile: 'plop-templates/component/Component.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{parentPath}}/{{name}}/index.ts',
+        templateFile: 'plop-templates/component/index.ts.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{parentPath}}/{{name}}/{{name}}.stories.tsx',
+        templateFile: 'plop-templates/component/Component.stories.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{parentPath}}/{{name}}/{{name}}.test.tsx',
+        templateFile: 'plop-templates/component/Component.test.tsx.hbs',
+      },
+    ],
+  });
+}
